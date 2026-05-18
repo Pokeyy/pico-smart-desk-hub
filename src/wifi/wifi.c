68a9f1ae1a0d6e09f5d33c2373a6db0eb9d2cfaf
Peter Tran <petertran241@gmail.com>
2026-05-18 01:06:42 -0700

    finished wifi task to account for reconnect issues and stuff

--- src/wifi/wifi.c
+++ src/wifi/wifi.c
@@ -6,10 +6,26 @@
 void wifi_task(void *pvParameters) {
     printf("Starting WTTR.in Pico W client...\n");
 
-    if (cyw43_arch_init())
-    {
-        printf("Failed to initialise CYW43\n");
-        return 1;
+    int cy43_count_MAX = 5;
+    int init_success = 0;
+    for(int i = 0; i < cy43_count_MAX; ++i) {
+        if (!cyw43_arch_init())
+        {
+            init_success = 1;
+            printf("cy43 initialized successfully\n");
+            break;
+        }
+        else {
+            printf("cy43 failed to intiialize.. trying again\n");
+            vTaskDelay(pdMS_TO_TICKS(3000));
+        }
+        
+        
+    }
+
+    if(init_success == 0) {
+        xEventGroupSetBits(wifi_group, 0x02);
+        vTaskDelete(NULL);
     }
 
     cyw43_arch_enable_sta_mode();
@@ -22,18 +38,22 @@ void wifi_task(void *pvParameters) {
     xEventGroupSetBits(wifi_group, 0x01);
     printf("Wi-Fi connected!\n");
 
+    
     while (true) {
         if (cyw43_tcpip_link_status(&cyw43_state, CYW43_ITF_STA) != CYW43_LINK_UP) {
             xEventGroupClearBits(wifi_group, 0x01);
             printf("Wi-Fi was lost, attempting to reconnect..\n");
-            while (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD,
-                                              CYW43_AUTH_WPA2_AES_PSK, 30000)) {
+            
+            if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD,
+                                            CYW43_AUTH_WPA2_AES_PSK, 30000) == 0) {
+                xEventGroupSetBits(wifi_group, 0x01);
+                printf("Wi-Fi reconnected!\n");                      
+            }
+            else {
                 printf("Reconnecting failed, trying a few more times..\n");
                 vTaskDelay(pdMS_TO_TICKS(5000));
             }
-            xEventGroupSetBits(wifi_group, 0x01);
-            printf("Wi-Fi reconnected!\n");
+            vTaskDelay(pdMS_TO_TICKS(20000));
         }
-        vTaskDelay(pdMS_TO_TICKS(5000));
     }
 }
--- src/wifi/wifi.h
+++ src/wifi/wifi.h
@@ -3,7 +3,10 @@
 #include "pico/cyw43_arch.h"
 #include "event_groups.h"
 
-EventGroupHandle_t wifi_group;
+#define WIFI_CONNECTED_BIT          0x01
+#define WIFI_FAILED_BIT             0x02
+
+extern EventGroupHandle_t wifi_group;
 
 void wifi_task(void *pvParameters);
 
